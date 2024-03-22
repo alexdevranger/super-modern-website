23d043a7107b46ad6e993be7b3da5af9d017676c
alexdevranger <aleksandra.vesic.dev@gmail.com>
2024-03-22 15:35:41 +0100

    added modal for announcement

--- src/components/Arabian.js
+++ src/components/Arabian.js
@@ -13,25 +13,91 @@ function Arabian() {
   return (
     <div className="flex w-full justify-center items-center">
       <div className="z-50">
-          <div className="relative">
-            {showModal && (
-              <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-20">
-                <div className="gradient-bg-welcome border border-[#fff] p-8 rounded-lg lg:w-1/2 px-16 text-white">
-                  <h2 className="mt-16 text-4xl font-bold my-4 text-center pb-12">NFT MARKETPLACE</h2>
-                  <p className="text-center text-xl font-semibold underline">📢 Important Announcement: New DUBX NFT Marketplace Coming Soon! 🚀</p>
-                  <p className="text-center mt-6">Dear users,</p>
-                  <p className="text-center mt-6">We are excited to announce that our highly anticipated DUBX NFT Marketplace will soon be launching, offering a wide range of unique digital collections and artworks.</p>
-                  <p className="text-center mt-6">Join our community and stay tuned for updates as we prepare to launch our Marketplace. Visit us soon and start your NFT journey with DUBX!</p>
-                  <p className="text-center mt-6">Additionally, we're thrilled to share that we are also preparing another <strong>groundbreaking application</strong> that will elevate standards and revolutionize the industry. Stay tuned for more details on this exciting new project!</p>
-                  <p className="text-center mt-6 font-semibold text-lg">Warm regards, <br />DUBX Team</p>
-                  <div className="flex w-full justify-end pt-16">
-                    <button className="px-4 py-2 bg-transparent w-full text-white hover:bg-white text-xl border rounded-md hover:text-black hover:border-cyan-400 transition-all duration-300 ease-in hover:font-semibold" onClick={closeModal}>CLOSE</button>
-                  </div>
+        <div className="relative">
+          {showModal && (
+            <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-20 overflow-y-auto">
+              <div className="bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 border border-white p-8 rounded-lg lg:w-1/2 w-full sm:max-w-lg px-6 text-white">
+                <h2 className="mt-32 md:mt-6 text-3xl font-bold my-4 text-center pb-8">
+                  NFT MARKETPLACE
+                </h2>
+                <p className="text-center text-lg font-semibold underline">
+                  📢 Important Announcement: New DUBX NFT Marketplace Coming
+                  Soon! 🚀
+                </p>
+                <p className="text-center mt-6">Dear users,</p>
+                <p className="text-center mt-6">
+                  We are excited to announce that our highly anticipated DUBX
+                  NFT Marketplace will soon be launching, offering a wide range
+                  of unique digital collections and artworks.
+                </p>
+                <p className="text-center mt-6">
+                  Join our community and stay tuned for updates as we prepare to
+                  launch our Marketplace. Visit us soon and start your NFT
+                  journey with DUBX!
+                </p>
+                <p className="text-center mt-6">
+                  Additionally, we're thrilled to share that we are also
+                  preparing another <strong>groundbreaking application</strong>{" "}
+                  that will elevate standards and revolutionize the industry.
+                  Stay tuned for more details on this exciting new project!
+                </p>
+                <p className="text-center mt-6 font-semibold text-base">
+                  Warm regards, <br />
+                  DUBX Team
+                </p>
+                <div className="flex justify-center mt-8">
+                  <button
+                    className="px-4 py-2 bg-[#1bf8ec] text-white hover:bg-[#e32970] text-lg font-semibold border border-transparent rounded-md transition duration-300 ease-in-out"
+                    onClick={closeModal}
+                  >
+                    CLOSE
+                  </button>
                 </div>
               </div>
-            )}
-          </div>
-    </div>
+            </div>
+            // <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-20 overflow-y-auto">
+            //   <div className="gradient-bg-welcome border border-[#fff] p-8 rounded-lg lg:w-1/2 px-16 text-white">
+            //     <h2 className="mt-16 text-4xl font-bold my-4 text-center pb-12">
+            //       NFT MARKETPLACE
+            //     </h2>
+            //     <p className="text-center text-xl font-semibold underline">
+            //       📢 Important Announcement: New DUBX NFT Marketplace Coming
+            //       Soon! 🚀
+            //     </p>
+            //     <p className="text-center mt-6">Dear users,</p>
+            //     <p className="text-center mt-6">
+            //       We are excited to announce that our highly anticipated DUBX
+            //       NFT Marketplace will soon be launching, offering a wide range
+            //       of unique digital collections and artworks.
+            //     </p>
+            //     <p className="text-center mt-6">
+            //       Join our community and stay tuned for updates as we prepare to
+            //       launch our Marketplace. Visit us soon and start your NFT
+            //       journey with DUBX!
+            //     </p>
+            //     <p className="text-center mt-6">
+            //       Additionally, we're thrilled to share that we are also
+            //       preparing another <strong>groundbreaking application</strong>{" "}
+            //       that will elevate standards and revolutionize the industry.
+            //       Stay tuned for more details on this exciting new project!
+            //     </p>
+            //     <p className="text-center mt-6 font-semibold text-lg">
+            //       Warm regards, <br />
+            //       DUBX Team
+            //     </p>
+            //     <div className="flex w-full justify-end pt-16">
+            //       <button
+            //         className="px-4 py-2 bg-transparent w-full text-white hover:bg-white text-xl border rounded-md hover:text-black hover:border-cyan-400 transition-all duration-300 ease-in hover:font-semibold"
+            //         onClick={closeModal}
+            //       >
+            //         CLOSE
+            //       </button>
+            //     </div>
+            //   </div>
+            // </div>
+          )}
+        </div>
+      </div>
       <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4 mt-20">
         <div className="flex-1 flex flex-col justify-start items-center w-[70%]">
           <LazyLoadImage
